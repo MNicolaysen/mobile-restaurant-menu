cc28817016680483ad3e6158ed8499b6fb5391ae
Morton Nicolaysen <mort1995@hotmail.com>
2023-03-15 09:28:26 +0000

    created loop for menu items to be displayed

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-import { menuArray } from ".data"
+import { menuArray } from "./data.js"
+
+console.log("hello")
+
+function getFeedHtml() {
+  let menuHtml = ``
+
+  menuArray.forEach(function(item){
+    console.log(item)
+    menuHtml += `
+    <div class="menu-item">
+      <div class="menu-item-full-info">
+        <img class="menu-item-icon" src="images/${item.name}.png">
+        <div class="menu-item-info">
+          <h2>${item.name}</h2>
+          <p>${item.ingredients}</p>
+          <h3>$${item.price}</h3>
+        </div>
+      </div>
+      <i>+</i>
+    </div>
+    <span class="spacer">_________________________________________________________________________</span>
+    `
+  })
+  return menuHtml
+}
+
+function render() {
+  document.getElementById('feed').innerHTML = getFeedHtml()
+}
+
+render()
